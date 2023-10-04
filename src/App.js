e65b128df292fbfbf2be63ffeb0459e93e52c2dd
Leonardo Genzolini <leonardogenzolini@gmail.com>
2023-10-04 12:23:26 +0200

    deploy

--- src/App.js
+++ src/App.js
@@ -1,92 +1,67 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+
+const audioURLs = {
+  Q: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+  W: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+  E: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+  A: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+  S: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+  D: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+  Z: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+  X: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+  C: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+};
 
 function App() {
   const [activeKey, setActiveKey] = useState('');
 
   useEffect(() => {
-    document.addEventListener('keydown', (event) => {
-      playSound(event.key.toUpperCase());
-    });
+    document.addEventListener('keydown', handleKeyDown);
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
   }, []);
 
-  const drumPads = [
-    {
-      keyCode: 81,
-      text: "Q",
-      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
-    },
-    {
-      keyCode: 87,
-      text: "W",
-      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
-    },
-    {
-      keyCode: 69,
-      text: "E",
-      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
-    },
-    {
-      keyCode: 65,
-      text: "A",
-      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
-    },
-    {
-      keyCode: 83,
-      text: "S",
-      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
-    },
-    {
-      keyCode: 68,
-      text: "D",
-      src: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
-    },
-    {
-      keyCode: 90,
-      text: "Z",
-      src: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
-    },
-    {
-      keyCode: 88,
-      text: "X",
-      src: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
-    },
-    {
-      keyCode: 67,
-      text: "C",
-      src: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+  function handleKeyDown(event) {
+    const key = event.key.toUpperCase();
+    if (audioURLs[key]) {
+      playSound(key);
     }
-  ];
+  }
 
   function playSound(selector) {
-    document.getElementById(selector).play();
-    setActiveKey(selector)
+    const audioElement = document.getElementById(selector);
+    if (audioElement) {
+      audioElement.play();
+      setActiveKey(selector);
+    }
   }
 
   return (
     <div className="App">
-    <div id='drum-machine'>
-      <div id="display">{ activeKey }</div>
-      <div className='drum-pads'>
-        {drumPads.map((drumPad) => (
-          <div
-            key={drumPad.src}
-            onClick={() => {
-              playSound(drumPad.text);
-            }}
-            className='drum-pad'
-            id={drumPad.src}
-          >
-            {drumPad.text}
-            <audio
-              className='clip'
-              id={drumPad.text}
-              src={drumPad.src}
-            ></audio>
-          </div>
-        ))}
+      <div id='drum-machine'>
+        <div id="display">Key selected: {activeKey}</div>
+        <div className='drum-pads'>
+          {Object.keys(audioURLs).map((key) => (
+            <div
+              key={key}
+              onClick={() => {
+                playSound(key);
+              }}
+              className='drum-pad'
+              id={audioURLs[key]}
+            >
+              {key}
+              <audio
+                className='clip'
+                id={key}
+                src={audioURLs[key]}
+              ></audio>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
     </div>
   );
 }
